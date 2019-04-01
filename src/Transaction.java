b689ccebc4768d75d5671237f32545524eab2ec0
Vinicel <Guillaume-cornet07@hotmail.fr>
2019-04-01 18:09:45 +0200

    Adding Transaction

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,5 @@
 import java.security.*;
+import java.util.ArrayList;
 
 public class Transaction {
 	
@@ -9,4 +10,24 @@ public class Transaction {
 	public byte[] signature;
 	
 	public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+	public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+	
+	private static int sequence = 0;
+	
+	public Transaction(PublicKey from, PublicKey to, float value,  ArrayList<TransactionInput> inputs {
+		this.sender = from;
+		this.reciepient = to;
+		this.value = value;
+		this.inputs = inputs;
+	}
+	
+	private String calulateHash() {
+		sequence++;
+		
+		return StringUtil.applySha256(
+				StringUtil.getStringFromKey(sender) +
+				StringUtil.getStringFromKey(reciepient) +
+				Float.toString(value) + sequence
+				); 
+	}
 }
